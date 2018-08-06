CarbonROM/android_build

build: bring back repopick, roomservice, cmremote and crremote
Change-Id: I8f120a26c7959999c8f1d89d0b2d6917c012095e

envsetup.sh
@@ -18,6 +18,9 @@ Invoke ". build/envsetup.sh" from your shell to add the following functions to y
 - sepgrep: Greps on all local sepolicy files.
 - sgrep:   Greps on all local source files.
 - godir:   Go to the directory containing a file.
+- cmremote: Add git remote for matching CM repository.
+- crremote: Add gerrit remote for matching Carbon repository.
+
 
 Environemnt options:
 - SANITIZE_HOST: Set to 'true' to use ASAN for all host modules. Note that
@@ -89,6 +92,44 @@ function check_variant()
     done
     return 1
 }
+
+function cmremote()
+{
+    git remote rm cm 2> /dev/null
+    if [ ! -d .git ]
+    then
+        echo .git directory not found. Please run this from the root directory of the Android repository you wish to set up.
+    fi
+    PROJECT=`pwd -P | sed s#$ANDROID_BUILD_TOP/##g`
+    PFX="android_$(echo $PROJECT | sed 's/\//_/g')"
+    git remote add cm git@github.com:CyanogenMod/$PFX
+    echo "Remote 'cm' created"
+}
+
+
+function crremote()
+{
+    git remote rm crremote 2> /dev/null
+    if [ ! -d .git ]
+    then
+        echo .git directory not found. Please run this from the root directory of the Android repository you wish to set up.
+    fi
+    GERRIT_REMOTE=$(cat .git/config  | grep git://github.com | awk '{ print $NF }' | sed s#git://github.com/##g)
+    if [ -z "$GERRIT_REMOTE" ]
+    then
+        echo Unable to set up the git remote, are you in the root of the repo?
+        return 0
+    fi
+    CRUSER=`git config --get review.review.carbonrom.org.username`
+    if [ -z "$CRUSER" ]
+    then
+        git remote add crremote ssh://review.carbonrom.org:29418/$GERRIT_REMOTE
+    else
+        git remote add crremote ssh://$CRUSER@review.carbonrom.org:29418/$GERRIT_REMOTE
+    fi
+    echo You can now push to "crremote".
+ }
+
 
 function setpaths()
 {
@@ -226,6 +267,12 @@ function printconfig()
         return
     fi
     get_build_var report_config
+}
+
+function repopick() {
+    set_stuff_for_environment
+    T=$(gettop)
+    $T/build/tools/repopick.py $@
 }
 
 function set_stuff_for_environment()
@@ -527,6 +574,20 @@ function lunch()
     check_product $product
     if [ $? -ne 0 ]
     then
+       # if we can't find the product, try to grab it from our github
+      T=$(gettop)
+      pushd $T > /dev/null
+      build/tools/roomservice.py $product
+      popd > /dev/null
+      check_product $product
+  else
+      T=$(gettop)
+      pushd $T > /dev/null
+      build/tools/roomservice.py $product true
+      popd > /dev/null
+  fi
+  if [ $? -ne 0 ]
+  then
         echo
         echo "** Don't have a product spec for: '$product'"
         echo "** Do you have the right repo manifest?"

CarbonBeta/android_build_make

envsetup.sh
@@ -20,6 +20,9 @@ Invoke ". build/envsetup.sh" from your shell to add the following functions to y
 - sepgrep:   Greps on all local sepolicy files.
 - sgrep:     Greps on all local source files.
 - godir:     Go to the directory containing a file.
+- cmremote: Add git remote for matching CM repository.
+- crremote: Add gerrit remote for matching Carbon repository.
+
 
 Environment options:
 - SANITIZE_HOST: Set to 'true' to use ASAN for all host modules. Note that
@@ -152,6 +155,44 @@ function check_variant()
     done
     return 1
 }
+
+function cmremote()
+{
+    git remote rm cm 2> /dev/null
+    if [ ! -d .git ]
+    then
+        echo .git directory not found. Please run this from the root directory of the Android repository you wish to set up.
+    fi
+    PROJECT=`pwd -P | sed s#$ANDROID_BUILD_TOP/##g`
+    PFX="android_$(echo $PROJECT | sed 's/\//_/g')"
+    git remote add cm git@github.com:CyanogenMod/$PFX
+    echo "Remote 'cm' created"
+}
+
+
+function crremote()
+{
+    git remote rm crremote 2> /dev/null
+    if [ ! -d .git ]
+    then
+        echo .git directory not found. Please run this from the root directory of the Android repository you wish to set up.
+    fi
+    GERRIT_REMOTE=$(cat .git/config  | grep git://github.com | awk '{ print $NF }' | sed s#git://github.com/##g)
+    if [ -z "$GERRIT_REMOTE" ]
+    then
+        echo Unable to set up the git remote, are you in the root of the repo?
+        return 0
+    fi
+    CRUSER=`git config --get review.review.carbonrom.org.username`
+    if [ -z "$CRUSER" ]
+    then
+        git remote add crremote ssh://review.carbonrom.org:29418/$GERRIT_REMOTE
+    else
+        git remote add crremote ssh://$CRUSER@review.carbonrom.org:29418/$GERRIT_REMOTE
+    fi
+    echo You can now push to "crremote".
+ }
+
 
 function setpaths()
 {
@@ -292,6 +333,12 @@ function printconfig()
         return
     fi
     get_build_var report_config
+}
+
+function repopick() {
+    set_stuff_for_environment
+    T=$(gettop)
+    $T/build/tools/repopick.py $@
 }
 
 function set_stuff_for_environment()
@@ -603,6 +650,20 @@ function lunch()
     fi
 
     if [ -z "$product" ]
+    then
+       # if we can't find the product, try to grab it from our github
+      T=$(gettop)
+      pushd $T > /dev/null
+      build/tools/roomservice.py $variant
+      popd > /dev/null
+      check_variant $variant
+    else
+      T=$(gettop)
+      pushd $T > /dev/null
+      build/tools/roomservice.py $variant true
+      popd > /dev/null
+    fi
+    if [ $? -ne 0 ]
     then
         echo
         echo "Invalid lunch combo: $selection"