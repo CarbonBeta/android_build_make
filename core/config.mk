9b736dc0f598bb893afd7a331cf698b261fa5ba3
Christian Oder <myself5@carbonrom.org>
2018-08-07 20:44:45 +0200

    REVERTME: Comment out sepolicy include
    Change-Id: I6470077c2fcb64574784744afd1a24dc7e6ba3d7

--- core/config.mk
+++ core/config.mk
@@ -1103,7 +1103,7 @@ endif
 ifneq ($(CARBON_BUILD),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-$(eval include device/carbon/sepolicy/common/sepolicy.mk)
+# $(eval include device/carbon/sepolicy/common/sepolicy.mk)
 
 # Include any vendor specific config.mk file
 -include $(TOPDIR)vendor/*/build/core/config.mk
